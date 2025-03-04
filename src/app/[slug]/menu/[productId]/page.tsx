e0310cf374ddde888417aa1ffc07c5dc9e3e0a5a
thiagolemos23 <thiago.00000844083@unicap.br>
2025-03-04 16:43:11 -0300

    feat: add scroll bar

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import ProductDetails from "./components/product-details";
 import ProductHeader from "./components/product-header";
 
 interface ProductPageProps {
--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
+import { formatCurrency } from "@/helpers/format-currency";
 
 interface ProductsProps {
   products: Product[];
@@ -24,7 +25,9 @@ const Products = ({ products }: ProductsProps) => {
             <p className="line-clamp-2 text-sm text-muted-foreground">
               {product.description}
             </p>
-            
+            <p className="pt-3 text-sm font-semibold">
+              {formatCurrency(product.price)}
+            </p>
           </div>
 
           {/* DIREITA */}
