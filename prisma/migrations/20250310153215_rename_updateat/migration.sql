f1958ffc5c9b6110c891f17301140327176b7e46
thiagolemos23 <thiago.00000844083@unicap.br>
2025-03-10 17:11:54 -0300

    renamed

---
+++ prisma/migrations/20250310153215_rename_updateat/migration.sql
@@ -0,0 +1,58 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updateAt` on the `Order` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `OrderProduct` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `Product` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `OrderProduct` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "MenuCategory" DROP CONSTRAINT "MenuCategory_restaurantId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Order" DROP CONSTRAINT "Order_restaurantid_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderProduct" DROP CONSTRAINT "OrderProduct_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderProduct" DROP CONSTRAINT "OrderProduct_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_menuCategoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Product" DROP CONSTRAINT "Product_restaurantId_fkey";
+
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "updateAt",
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "OrderProduct" DROP COLUMN "updateAt",
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "updateAt",
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "MenuCategory" ADD CONSTRAINT "MenuCategory_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_menuCategoryId_fkey" FOREIGN KEY ("menuCategoryId") REFERENCES "MenuCategory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_restaurantid_fkey" FOREIGN KEY ("restaurantid") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
