928ca5f3b174c21f54b8261700a5102c816f9078
thiagolemos23 <thiago.00000844083@unicap.br>
2025-03-11 15:11:11 -0300

    Redirecionando

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here*/
   images: {
     remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/fsw-donalds", // Altere para a rota correta do seu projeto
+        permanent: false, // Defina como true se quiser um redirecionamento permanente (301)
+      },
+    ];
   },
 };
 
